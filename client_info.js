be7bb4e5965c81965a43164b573d7bdb33f26c33
Rithik <rishabh97dutt@gmail.com>
2020-11-17 10:32:12 +0530

    deploying..

--- client_info.js
+++ client_info.js
@@ -3,6 +3,6 @@ module.exports = {
 
     CLIENT_SECRET : "L_MqptIpon_259a75sd4qNKcnHeYbzTu",
 
-    TOKEN : "Nzc2Njc5MjA3MDkwMzg4OTkz.X64Y4A.Z6oxJLhXM5v5prYVqXpy5LyLYFU"
+    TOKEN : "Nzc2Njc5MjA3MDkwMzg4OTkz.X64Y4A.dvVCToJhvy3JVJHobzVkObT-kmI"
 
 };
