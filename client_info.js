6452866c16749d709f4fc1c983ed7698ad6a7cc7
Rithik <duttrithik@gmail.com>
2021-01-03 17:30:07 +0530

    trigger added 3
    Signed-off-by: Rithik <duttrithik@gmail.com>

--- client_info.js
+++ client_info.js
@@ -3,6 +3,6 @@ module.exports = {
 
     CLIENT_SECRET : "L_MqptIpon_259a75sd4qNKcnHeYbzTu",
 
-    TOKEN : "Nzc2Njc5MjA3MDkwMzg4OTkz.X64Y4A.X_INSxjEnePG_NU_X-cSi2iratY"
+    TOKEN : "Nzc2Njc5MjA3MDkwMzg4OTkz.X64Y4A.iC-0PFqsoj4lO0pCkWi35v5sw98"
 
 };
